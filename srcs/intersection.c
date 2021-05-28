f28274aef60401728e3fefbb4e0822e83c990cb1
Rustam @SecondFry Gubaydullin <secondfry+github@gmail.com>
2021-05-29 00:41:21 +0300

    [intersection.c] norminette

--- srcs/intersection.c
+++ srcs/intersection.c
@@ -23,6 +23,18 @@ t_byte	is_wall(t_wolf *wolf, unsigned short x, unsigned short y)
 	return (wolf->map[wolf->map_width * y + x] == '1');
 }
 
+static void	horizontal_increment(float *wall_x, float *wall_y, float angle)
+{
+	if (angle > -90 && angle < 90)
+		*wall_y -= 64;
+	else
+		*wall_y += 64;
+	if (angle > 0 && angle < 180)
+		*wall_x += 64 * fabsf(tanf(angle * M_PI_F / 180));
+	else
+		*wall_x -= 64 * fabsf(tanf(angle * M_PI_F / 180));
+}
+
 t_point	intersection_horizontal(t_wolf *wolf, float angle, float wall_y)
 {
 	float			dy;
@@ -33,32 +45,30 @@ t_point	intersection_horizontal(t_wolf *wolf, float angle, float wall_y)
 	if ((90 - EPSILON < angle && angle < 90 + EPSILON)
 		|| (-90 - EPSILON < angle && angle < -90 + EPSILON))
 		return ((t_point){0, 0});
-	
 	dy = wolf->player->pos.y - wall_y;
 	dx = dy * tanf(angle * M_PI_F / 180);
 	wall_x = dx + wolf->player->pos.x;
-	check = is_wall(wolf, wall_x, wall_y); // FIXME blyat
+	check = is_wall(wolf, wall_x, wall_y);
+	while (check == 0)
+	{
+		horizontal_increment(&wall_x, &wall_y, angle);
+		check = is_wall(wolf, wall_x, wall_y);
+	}
 	if (check == 1)
 		return ((t_point){wall_x, wall_y});
-	if (check == 2)
-		return ((t_point){0, 0});
-	
-	while (1)
-	{
-		if (angle > -90 && angle < 90)
-			wall_y -= 64;
-		else
-			wall_y += 64;
-		if (angle > 0 && angle < 180)
-			wall_x += 64 * fabsf(tanf(angle * M_PI_F / 180));
-		else
-			wall_x -= 64 * fabsf(tanf(angle * M_PI_F / 180));
-		check = is_wall(wolf, wall_x, wall_y);
-		if (check == 1)
-			return ((t_point){wall_x, wall_y});
-		if (check == 2)
-			return ((t_point){0, 0});
-	}
+	return ((t_point){0, 0});
+}
+
+static void	vertical_increment(float *wall_x, float *wall_y, float angle)
+{
+	if (angle > 0 && angle < 180)
+		*wall_x += 64;
+	else
+		*wall_x -= 64;
+	if (angle > -90 && angle < 90)
+		*wall_y -= 64 / fabsf(tanf(angle * M_PI_F / 180));
+	else
+		*wall_y += 64 / fabsf(tanf(angle * M_PI_F / 180));
 }
 
 t_point	intersection_vertical(t_wolf *wolf, float angle, float wall_x)
@@ -72,30 +82,16 @@ t_point	intersection_vertical(t_wolf *wolf, float angle, float wall_x)
 		|| (-180 - EPSILON < angle && angle < -180 + EPSILON)
 		|| (180 - EPSILON < angle && angle < 180 + EPSILON))
 		return ((t_point){0, 0});
-
 	dx = wolf->player->pos.x - wall_x;
 	dy = dx / tanf(angle * M_PI_F / 180);
 	wall_y = dy + wolf->player->pos.y;
 	check = is_wall(wolf, wall_x, wall_y);
+	while (check == 0)
+	{
+		vertical_increment(&wall_x, &wall_y, angle);
+		check = is_wall(wolf, wall_x, wall_y);
+	}
 	if (check == 1)
 		return ((t_point){wall_x, wall_y});
-	if (check == 2)
-		return ((t_point){0, 0});
-
-	while (1)
-	{
-		if (angle > 0 && angle < 180)
-			wall_x += 64;
-		else
-			wall_x -= 64;
-		if (angle > -90 && angle < 90)
-			wall_y -= 64 / fabsf(tanf(angle * M_PI_F / 180));
-		else
-			wall_y += 64 / fabsf(tanf(angle * M_PI_F / 180));
-		check = is_wall(wolf, wall_x, wall_y);
-		if (check == 1)
-			return ((t_point){wall_x, wall_y});
-		if (check == 2)
-			return ((t_point){0, 0});
-	}
+	return ((t_point){0, 0});
 }
